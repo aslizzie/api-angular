243084e0e8117658501cffd3aaa4053dcb9686f5
Diana Alcala <diana.alcalasandoval@gmail.com>
2024-07-28 23:11:15 -0600

    Limpieza de config

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
+// Configurar lo siguiente de acuerdo a nuestro servidor 
 const pool = mysql.createPool({
-    host: 'localhost',  // Cambia esto por la dirección de tu servidor MySQL
-    user: 'root',       // Tu usuario de MySQL
-    password: 'root',  // Tu contraseña de MySQL
-    database: 'wave'  // El nombre de tu base de datos
+    host: '',
+    user: '',
+    password: '',
+    database: ''
 });
 
-module.exports = pool.promise();  // Usamos .promise() para habilitar el uso de promesas
+module.exports = pool.promise(); 
